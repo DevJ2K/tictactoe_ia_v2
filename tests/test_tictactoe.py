5ecddfbbfcd2c4d898069f5ae1e0e9521bff7b66
DevJ2K <ajavontheopro@gmail.com>
2024-10-28 12:48:19 +0100

    ✅ : Create tests for terminal state.

--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -10,3 +10,23 @@ def test_player_turn():
 	assert TicTacToe(who_start='O').get_player_turn([["X", "O", " "], [" ", " ", " "], [" ", " ", " "]]) == 'O'
 	assert TicTacToe(who_start='X').get_player_turn([["X", "O", "X"], [" ", " ", " "], [" ", " ", " "]]) == 'O'
 	assert TicTacToe(who_start='O').get_player_turn([["X", "O", "O"], [" ", " ", " "], [" ", " ", " "]]) == 'X'
+
+def test_terminal_state_true():
+	# Horizontal
+	assert TicTacToe().terminal_state([["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]]) == True
+	assert TicTacToe().terminal_state([[" ", " ", " "], ["X", "X", "X"], [" ", " ", " "]]) == True
+	assert TicTacToe().terminal_state([[" ", " ", " "], [" ", " ", " "], ["X", "X", "X"]]) == True
+
+	# Vertical
+	assert TicTacToe().terminal_state([["X", " ", " "], ["X", " ", " "], ["X", " ", " "]]) == True
+	assert TicTacToe().terminal_state([[" ", "X", " "], [" ", "X", " "], [" ", "X", " "]]) == True
+	assert TicTacToe().terminal_state([[" ", " ", "X"], [" ", " ", "X"], [" ", " ", "X"]]) == True
+
+	# Diagonal
+	assert TicTacToe().terminal_state([["X", " ", " "], [" ", "X", " "], [" ", " ", "X"]]) == True
+	assert TicTacToe().terminal_state([[" ", " ", "X"], [" ", "X", " "], ["X", " ", " "]]) == True
+
+def test_terminal_state_false():
+	assert TicTacToe().terminal_state([[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]) == False
+	assert TicTacToe().terminal_state([[" ", " ", " "], [" ", "X", "X"], [" ", " ", " "]]) == False
+	assert TicTacToe().terminal_state([["X", " ", " "], [" ", "X", "X"], [" ", " ", " "]]) == False
